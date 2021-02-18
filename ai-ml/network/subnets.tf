a7fa212d8cbc6a08dfdacadac571fa43f74d8fd1
Dexin Zhong <dexinz@hotmail.com>
2021-02-18 18:42:10 -0500

    commit

--- ai-ml/network/subnets.tf
+++ ai-ml/network/subnets.tf
@@ -1,6 +1,6 @@
 resource "oci_core_subnet" "Mset2Subnet" {
   availability_domain = ""
-  cidr_block          = "10.1.0.0/24"
+  cidr_block          = "10.1.1.0/24"
   display_name        = "Mset2Subnet"
   dns_label           = "mset2"
   security_list_ids   = ["${oci_core_security_list.Mset2SecurityList.id}"]
